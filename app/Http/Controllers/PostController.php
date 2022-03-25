0e7698b68a75efdacc414c65fd0c78311678378d
awgst <trisaktiawang@gmail.com>
2022-03-25 19:38:20 +0700

    Create post fo a particular website

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\Website;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
-    //
+    public function store(Request $request, $websiteId)
+    {
+        try {
+            $website = Website::find($websiteId);
+            if (is_null($website)) {
+                throw new Exception('Website not found');
+            }
+
+            $validator = Validator::make($request->all(), [
+                'title' => 'required',
+                'description' => 'required'
+            ]);
+     
+            if ($validator->fails()) {
+                return response()->json([
+                    'message' => 'Error, the given data is invalid', 
+                    'errors' => $validator->getMessageBag()->getMessages()
+                ]);
+            }
+            $data = $validator->validated();
+            $website->posts()->create($data);
+            $message = 'Post successfully created';
+        } catch (Exception $e) {
+            $message = $e->getMessage();       
+        }
+
+        return response()->json(['message' => $message], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group(['prefix' => 'website'], function (){
+    Route::get('test', function (){
+        return response()->json(['message'=>'test']);
+    });
+    Route::post('{websiteId}/post', [PostController::class, 'store']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
 });
